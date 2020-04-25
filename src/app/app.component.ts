b96bb2081b6f4c509ed8ade232f048b79d9314be
Vishwa Raj <qwertyasvishwa@outlook.com>
2020-04-25 16:51:45 +0530

    Created component and objects under the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LearnAngularScratch';
+  title = 'Learn Angular';
+  body = "Hello World"
+  myObject = {
+    gender : 'male',
+    age: 33,
+    location : "India"
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNewComponentComponent
   ],
   imports: [
     BrowserModule,
